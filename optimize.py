59f2d5116ceb534510dae8d9774afd51d2464a37
TShapinsky <tobiasshapinsky@gmail.com>
2021-11-08 12:34:05 -0700

    first pass at function optimization

--- optimize.py
+++ optimize.py
@@ -16,9 +16,12 @@ class LoanOptimizer:
         loans = [copy.copy(loan) for loan in self.loans]
         date = copy.copy(self.start_date)
         total_paid = 0
+        #print(args)
         args = args[0]
         initial_splits = args[0:len(self.loans)-1]
+        #print(initial_splits)
         period_splits = args[len(self.loans)-1:]
+        period_splits = np.reshape(period_splits, (len(initial_splits), int(len(period_splits)/len(initial_splits))))
 
         initial_paydown_amounts = self.splits_to_amounts(initial_paydown, initial_splits)
         loan_history = []
@@ -28,9 +31,10 @@ class LoanOptimizer:
             loan_history.append([loan.get_balance()])
             total_paid = total_paid + loan.pay(initial_paydown_amounts[idx])
 
-        period_payment_amounts = self.splits_to_amounts(period_payment, period_splits)
-
+        i = 0
         while date < self.start_date + relativedelta(years=10):
+            period_payment_amounts = self.function_splits_to_amounts(period_payment, period_splits, i)
+            #print(period_payment_amounts)
             date = date+ self.payment_period
             period_paid = 0
             for idx, loan in enumerate(loans):
@@ -46,8 +50,10 @@ class LoanOptimizer:
                 if graph:
                     for loan_values in loan_history:
                         plt.plot(loan_values)
+                        print(loan_values)
                     plt.savefig("balances_over_time.png")
                 return (total_paid, date - self.start_date)
+            i+= 1
 
         remaining = 0
 
@@ -88,17 +94,33 @@ class LoanOptimizer:
         amounts.append(amount)
         return amounts
 
+    def function_splits_to_amounts(self, amount, function_splits, x):
+        amounts = []
+        for function_split in function_splits:
+            split = function_split[0] + function_split[1]*(x-function_split[2])
+            #for idx, c in enumerate(function_split):
+            #    split += c*(x**idx)
+            #print(split)
+            split = min(max(split,0), 1)
+            specific_amount = amount*split
+            amounts.append(specific_amount)
+            amount = amount - specific_amount
+        amounts.append(amount)
+        return amounts
+
     def optimize(self, initial_paydown, period_payment):
         def optimize_simulate(*args):
             (total_paid, payment_time) = self.simulate(initial_paydown, period_payment, *args)
             return total_paid
 
         params = [(0.0,1.0)]*(len(self.loans)*2 - 2)
-        res = gp_minimize(optimize_simulate, [(0.0, 1.0)]*(len(self.loans)*2 - 2), n_calls=200, x0=[0.5]*(len(self.loans)*2 - 2), n_initial_points = 50)
+        split_count = len(self.loans) -1
+        res = gp_minimize(optimize_simulate, [(0.0, 1.0)]*split_count + [(0.0,1.0),(-1.0,1.0),(-100.0,100.0)]*split_count, n_calls=200, x0=[0.5]*split_count + [0.5,0,0]*split_count, n_initial_points = 20, acq_optimizer="lbfgs", n_jobs=-1)
         initial_splits = res.x[0:len(self.loans)-1]
         period_splits = res.x[len(self.loans)-1:]
+        period_splits = np.reshape(period_splits, (len(initial_splits), int(len(period_splits)/len(initial_splits))))
         initial_paydown_amounts = self.splits_to_amounts(initial_paydown, initial_splits)
-        period_payment_amounts = self.splits_to_amounts(period_payment, period_splits)
+        period_payment_amounts = self.function_splits_to_amounts(period_payment, period_splits, 0)
         (total_paid, payment_time) = self.simulate(initial_paydown, period_payment, res.x, graph=True)
         return (total_paid, payment_time, initial_paydown_amounts, period_payment_amounts)
     
